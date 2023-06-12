37aac1279470d39a5b9b21d95e5ea0374cc634b1
Luiz felipe Machado <88983072+Felpszinho@users.noreply.github.com>
2023-06-12 15:10:51 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+    
+
+    $("#telefone").mask('(00) 00000-0000')
+  
+    
+    
+ 
+    
+
+    $('form').validate({
+  
+
+        rules: {
+           nome:{
+            required: true,
+            
+
+           },
+           email:{
+            required: true,
+            email: true
+
+           },
+           telefone:{
+            required: true,
+          
+            
+           },
+       
+
+        },
+
+        messages: {
+            nome:'ensira seu nome ',
+            
+            telefone:'ensira seu telefone',
+            email:'seu email é inválido',           
+          
+
+        },
+        
+
+
+    })
+});
+
